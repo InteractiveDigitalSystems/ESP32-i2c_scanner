daaa5d924696c5f715c4db05a52dbbda1519524e
EbbeVang <ev@evang.dk>
2020-09-24 08:58:46 +0200

    cleaned up comments and added ports

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,3 @@
-/*
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(500);
-  Serial.print("hello world");
-}
-*/
-
 /*********
   Rui Santos
   Complete project details at https://randomnerdtutorials.com  
